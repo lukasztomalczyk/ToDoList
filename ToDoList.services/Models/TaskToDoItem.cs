36df8a44bf30d0c88072ff87464894fd075fe726
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-06-11 18:46:35 +0200

    Make database and insert record to them

---
+++ ToDoList.services/Models/TaskToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.services.Models
+{
+    public class TaskToDoItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
