1d2f0520bcf274b09e71e771ee3f787a2c98bf1d
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-06-12 16:25:01 +0200

    make mapping class of TaskToDo entity

---
+++ ToDoList.services/Mappings/TaskToDoItemMap.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using NHibernate.Mapping;
+using ToDoList.services.Models;
+
+namespace ToDoList.services.Mappings
+{
+    /// <summary>
+    ///  Here's the mapping class, mapping TaskToDoItem
+    /// </summary>
+    public class TaskToDoItemMap : ClassMap<TaskToDoItem>
+    {
+        public TaskToDoItemMap()
+        {
+            Table("Task");
+                
+            Id(x => x.ID);
+            Map(x => x.Name)
+                .Not.Nullable();
+            Map(x => x.Description)
+                .Nullable();
+            Map(x => x.IsDone);   
+        }
+    }
+}
