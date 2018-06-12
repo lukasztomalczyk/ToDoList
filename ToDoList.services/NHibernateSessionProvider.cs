1136cc2aef3c672d3f7cc998791d8386cdbee70b
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-06-12 16:40:53 +0200

    Make class responsible for open connection to database

---
+++ ToDoList.services/NHibernateSessionProvider.cs
@@ -0,0 +1,31 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using ToDoList.services.Models;
+
+namespace ToDoList.services
+{
+    public static class NHibernateSessionProvider
+    {
+        public static ISession OpenSessionNHibernate()
+        {
+            ISessionFactory sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012
+                    .ConnectionString("")
+                    .ShowSql())
+
+                // Mapping our entity
+                .Mappings(m => m.FluentMappings
+                    .AddFromAssemblyOf<TaskToDoItem>())
+                
+                // Create your schema at runtime.
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                    .Create(false, false))
+                .BuildSessionFactory();
+
+            return sessionFactory.OpenSession();
+
+        }
+    }
+}
