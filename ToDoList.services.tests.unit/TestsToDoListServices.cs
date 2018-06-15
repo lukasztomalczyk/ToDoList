721def82c2e7384823958ed989069a95ec1c2aa4
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-06-15 18:50:18 +0200

    refactoring

--- ToDoList.services.tests.unit/TestsToDoListServices.cs
+++ ToDoList.services.tests.unit/TestsToDoListServices.cs
@@ -13,21 +13,21 @@ namespace ToDoList.services.tests.unit
     [TestFixture]
     public class TestsToDoListServices
     {
-        private readonly IEnumerable<TaskToDoItem> _fakeDataBase;
-
-        public TestsToDoListServices()
-        {
-            _fakeDataBase = new List<TaskToDoItem>()
+        private readonly IEnumerable<TaskToDoItem> _fakeDataBase = new List<TaskToDoItem>()
             {
                 new TaskToDoItem() { Id = 1, Name = "name_1", Description = "description_1", CreationDate = new DateTime(2018,01,10), DateOfCompletion = new DateTime(2018,02,10), IsDone = false },
                 new TaskToDoItem() { Id = 2, Name = "name_2", Description = "description_2", CreationDate = new DateTime(2018,01,11), DateOfCompletion = new DateTime(2018,02,11), IsDone = false },
             };
-        }
+        
 
         [Test]
         public void GetById_RetrievesUserID_SchouldReturnInt()
         {
+            // Arrange
             
+            // Act
+            
+            // Assert
         }
     }
 }
