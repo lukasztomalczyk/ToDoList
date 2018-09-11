d865894112b3c07eaf7365483dc44ceb8c47308a
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-09-11 09:25:22 +0200

    initial new database mongodb

---
+++ ToDoList.Database/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToDoList.Database
+{
+    public class Class1
+    {
+    }
+}
---
+++ ToDoList.services/DI/ServicesCollection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToDoList.services.FluentHibernate;
+
+namespace ToDoList.services.DI
+{
+    public static class ServicesCollection
+    {
+        public static void AddCollection(this IServiceCollection services)
+        {
+/*
+            var builder = new ContainerBuilder();
+            builder.RegisterType<NHibernateSessionProvider>();
+            var container = builder.Build();
+
+            var scope = container.BeginLifetimeScope();
+            scope.Resolve<NHibernateSessionProvider>();
+            */
+
+        }
+    }
+}
