4dc035a8f8994d016abf3c33c0d8b4790b5765dc
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-09-11 09:33:03 +0200

    change framework from 2.0 t 2.1 for all project

--- ToDoList.services/DI/ServicesCollection.cs
+++ ToDoList.services/DI/ServicesCollection.cs
@@ -6,15 +6,7 @@ namespace ToDoList.services.DI
     {
         public static void AddCollection(this IServiceCollection services)
         {
-/*
-            var builder = new ContainerBuilder();
-            builder.RegisterType<NHibernateSessionProvider>();
-            var container = builder.Build();
-
-            var scope = container.BeginLifetimeScope();
-            scope.Resolve<NHibernateSessionProvider>();
-            */
-
+            var settings =  new Mong
         }
     }
 }
